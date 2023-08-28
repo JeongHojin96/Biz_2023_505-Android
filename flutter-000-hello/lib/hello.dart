db6d36b8aa4e9dd6e6e81599c587d1fddaa76353
JeongHojin96 <JeongHojin96@JeongHojin96.com>
2023-08-28 15:41:44 +0900

    and

---
+++ flutter-000-hello/lib/hello.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const Center(
+    child: Text(
+      "반갑소",
+      style: TextStyle(
+        fontSize: 30,
+        color: Color.fromRGBO(255, 16, 199, 1),
+      ),
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
