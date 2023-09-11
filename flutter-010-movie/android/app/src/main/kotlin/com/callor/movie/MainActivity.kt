9df0fec387d82b1b907e04905fe6cf6dc10ab8d0
LazyNeco <130424032+JeongHojin96@users.noreply.github.com>
2023-09-11 14:09:58 +0900

    Delete flutter-010-movie directory

--- flutter-010-movie/android/app/src/main/kotlin/com/callor/movie/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.callor.movie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter-010-movie/lib/api/naver_open_api.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const CLIENT_ID = "12345";
-const CLIENT_SECRET = "12345";
-
-class NaverAPI {}
