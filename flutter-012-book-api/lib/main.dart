86a7be16f1d950cc0c1b97d9aa9d6a2dac43377b
JeongHojin96 <JeongHojin96@JeongHojin96.com>
2023-09-13 15:41:28 +0900

    naverapi

--- flutter-012-book-api/lib/main.dart
+++ flutter-012-book-api/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:book/api/naver_open_api.dart';
+import 'package:book/models/naver_book_dto.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
@@ -9,11 +12,78 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ViewBooksPage(),
+    );
+  }
+}
+
+class ViewBooksPage extends StatefulWidget {
+  ViewBooksPage({super.key});
+
+  final searchInputController = TextEditingController();
+
+  @override
+  State<ViewBooksPage> createState() => _ViewBooksPageState();
+}
+
+class _ViewBooksPageState extends State<ViewBooksPage> {
+  Future<List<NaverBookDto>>? resultBooks = NaverOpenAPI().loadBooks();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+              child: TextField(
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.search,
+                onSubmitted: (value) {},
+                decoration: const InputDecoration(
+                    labelText: "검색어",
+                    border: OutlineInputBorder(),
+                    hintText: "검색어 입력"),
+              ),
+            ),
+          ],
         ),
+      ),
+      body: ViewListPage(resultBooks: resultBooks),
+    );
+  }
+}
+
+class ViewListPage extends StatelessWidget {
+  const ViewListPage({
+    super.key,
+    required this.resultBooks,
+  });
+
+  final Future<List<NaverBookDto>>? resultBooks;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: FutureBuilder(
+        future: resultBooks,
+        builder: (context, snapshot) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: CircleAvatar(
+                // child: Image.network(snapshot.data![index].image!),
+                backgroundImage: NetworkImage(snapshot.data![index].image!),
+              ),
+              title: Text(snapshot.data![index].title!),
+              subtitle: Text(snapshot.data![index].author!),
+            ),
+          );
+        },
       ),
     );
   }
