86258e84d6816d8ce89def19cef339c22fec2fb6
JeongHojin96 <JeongHojin96@JeongHojin96.com>
2023-09-19 13:21:55 +0900

    Gw

--- flutter-020-data-go/lib/main.dart
+++ flutter-020-data-go/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:api/api/bus_station.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+  BusStation().loadStation();
   runApp(const MainApp());
 }
 
